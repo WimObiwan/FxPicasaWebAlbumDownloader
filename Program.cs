ad400fa13ca967cdd0953a2e805bd3b0eacae9f8
Wim Devos <wimobiwan@gmail.com>
2015-10-22 21:20:41 +0200

    Added Mono.Posix to output for Windows

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Gtk;
+using System.Text;
 
 namespace FxPicasaDownloader
 {
@@ -7,10 +8,19 @@ namespace FxPicasaDownloader
 	{
 		public static void Main (string[] args)
 		{
+			AppDomain.CurrentDomain.UnhandledException += AppDomain_CurrentDomain_UnhandledException;
 			Application.Init ();
 			MainWindow win = new MainWindow ();
 			win.Show ();
 			Application.Run ();
+		}
+
+		static void AppDomain_CurrentDomain_UnhandledException (object sender, UnhandledExceptionEventArgs e)
+		{
+			MessageDialog md = new MessageDialog (null, DialogFlags.Modal, MessageType.Error, ButtonsType.OkCancel, 
+				                   e.ExceptionObject.ToString ()); 
+			md.Run ();
+			md.Destroy ();
 		}
 	}
 }
